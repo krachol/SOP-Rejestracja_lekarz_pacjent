9b1932dba03366faf57ed41f079d0cc6cc4ad5bc
Mateusz Krasicki <mateusz.krasicki@gmail.com>
2016-02-19 21:39:30 +0100

    Initial commit

---
+++ src/Shared/header.h
@@ -0,0 +1,17 @@
+#ifndef HEADER_H_9HNW3YFM
+#define HEADER_H_9HNW3YFM
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+
+void printResult(double input, double output); 
+
+
+#endif /* end of include guard: HEADER_H_9HNW3YFM */
---
+++ src/Shared/printer.c
@@ -0,0 +1,15 @@
+#include "header.h"
+
+
+void printResult(double input, double output) {
+
+    fprintf(stdout,"The square root of %g is %g\n",
+            input, 
+            output);
+    return;
+}
+
+int asd() 
+{ 
+    return 1; 
+}

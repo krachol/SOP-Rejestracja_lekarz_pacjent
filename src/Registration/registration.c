5aca4401748e8d6442f608b44aeeca1580d83044
Mateusz Krasicki <mateusz.krasicki@gmail.com>
2016-02-20 10:35:18 +0100

    Fix unused variable warning in Registration
    Fix warning by commenting out now unused line. It's left for future use.

--- src/Registration/registration.c
+++ src/Registration/registration.c
@@ -3,11 +3,12 @@
 
 int main ()
 {
-    int input_msg_ipc_id = msgget(12345, IPC_CREAT | IPC_EXCL);
+    /* int input_msg_ipc_id = msgget(12345, IPC_CREAT | IPC_EXCL); */
     int end = 0;
 
     while (!end) {
-        
+        end = 1;
+
     }
         
     return 0;
